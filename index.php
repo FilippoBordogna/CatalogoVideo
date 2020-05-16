8a1c11266a77751f1103502701c8da6141e1cce6
barcigabri <33417277+barcigabri@users.noreply.github.com>
2020-05-16 13:14:51 +0200

    Sistemata login e aggiunto copertine film

--- index.php
+++ index.php
@@ -173,7 +173,7 @@
 				echo ('
 					<div class="col-md-4">
 					  <div class="card mb-4 shadow-sm">
-						<img src="images/'.$riga["id"].'.jpg" class="bd-placeholder-img card-img-top" width="100%" height="100%"  focusable="false" role="img" aria-label="Placeholder: Thumbnail">
+						<img src="images/video/'.$riga["id"].'.jpg" class="bd-placeholder-img card-img-top" width="100%" height="100%"  focusable="false" role="img" aria-label="Placeholder: Thumbnail">
 						<div class="card-body">
 						  <p class="card-text">'.$riga["nome"].'</p>
 						  <div class="d-flex justify-content-between align-items-center">
