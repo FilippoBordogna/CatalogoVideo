ea5ee5e7efa5b1285a3be892e617fe830956b644
FilippoBordogna <bordognapippo99@gmail.com>
2020-05-20 20:13:45 +0200

    Aggiunta tabelle generi e generi video e minimo poplamento

--- catalogo.sql
+++ catalogo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 20, 2020 alle 03:09
+-- Creato il: Mag 20, 2020 alle 20:12
 -- Versione del server: 10.1.31-MariaDB
 -- Versione PHP: 7.2.4
 
